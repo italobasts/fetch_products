06f692ee057a659c260e96d9cccd00c70e594ef9
Italo Moreira <italo@dimensiva.online>
2022-11-13 09:15:47 -0300

    changed title name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Consumir produtos',
+      title: 'Fetch Products',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
